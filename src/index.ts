d87a4bdd8a747c3024187d3d7cedec28271e5f69
gglennd <grdimaano18@gmail.com>
2025-06-08 12:46:42 +0800

    Added onError and notFound handler

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Hono } from "hono";
 
+import notFound from "@/not-found";
+import onError from "@/on-error";
+
 const app = new Hono();
 
 app.get("/", c => c.text("Hono!"));
+
+app.onError(onError);
+app.notFound(notFound);
 
 export default app;
